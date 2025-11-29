102910dbfff80e119dc21d745bee57b6d58689bc
Suraj Pathak <pathaksuraj580@gmail.com>
2025-11-30 01:10:37 +0530

    Update url of get food list

--- Tomato/frontend/src/Context/StoreContex.jsx
+++ Tomato/frontend/src/Context/StoreContex.jsx
@@ -5,7 +5,7 @@ export const StoreContext = createContext();
 const StoreContextProvider = (props) => {
   const [food_list, setFoodList] = useState([]);
   const [cartItems, setCartItems] = useState({});
-  const url = "http://localhost:4000";
+  const url = "https://fooddeliveryapp-sup4.onrender.com";
   const [token, setToken] = useState("");
 
   const addToCart = async (itemId) => {
