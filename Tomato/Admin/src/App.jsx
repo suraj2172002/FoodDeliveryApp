fdbd5a185e8718e4a90ebac694effc9a835278cb
Suraj Pathak <pathaksuraj580@gmail.com>
2025-11-30 01:38:58 +0530

    update admin render url

--- Tomato/Admin/src/App.jsx
+++ Tomato/Admin/src/App.jsx
@@ -9,7 +9,7 @@ import {ToastContainer} from "react-toastify"
 import "react-toastify/dist/ReactToastify.css"
 
 const App = () => {
-  const url="https://fooddeliveryapp-sup4.onrender.com/";
+  const url="https://fooddeliveryapp-sup4.onrender.com";
   return (
     <div>
       <ToastContainer/>
